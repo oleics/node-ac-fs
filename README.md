2dcdc555f915ebfce585cfcde2573ce4b45cc787
Oliver Leics <oliver.leics@gmail.com>
2016-06-22 22:26:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+
+ac-fs
+==========
+
+Common and/or convenient file-system functions returning promises.
+
+Install
+-------
+
+```sh
+npm install ac-fs --save
+```
+
+Usage
+-----
+
+```
+var fs = require('ac-fs');
+
+fs.readFolder('.')
+  .then(function(files){
+    // Remove all files
+    return Promise.all(files.map(function(file){
+      return fs.unlink(file);
+    }));
+  })
+;
+```
+
+API
+---
+
+### Paths
+
+exists(path) -> boolean
+
+### Files
+
+isFile(path) -> boolean
+
+isFileStrict(path) -> boolean
+
+readFile(path) -> instance of Buffer
+
+readJsonFile(path) -> parsed data
+
+writeFile(path, data)
+
+writeJsonFile(path, data)
+
+unlink(path)
+
+### Folders
+
+isFolder(path) -> boolean
+
+isFolderStrict(path) -> boolean
+
+readFolder(path) -> array of files
+
+mkFolder(path)
+
+rmFolder(path)
+
+rimrafFolder(path)
+
+rmEmptyFolder(path) -> boolean
+
+rmEmptyFolderTail(path)
+
+### Symbolic Links
+
+symlink(linkTo, path [, type])
+
+NOTICE `type` defaults to 'junction' for windows-platforms.
+See [fs.symlink()](https://nodejs.org/api/fs.html#fs_fs_symlink_target_path_type_callback).
+
+readlink(path) -> linkString
+
+isSymlink(path) -> boolean
+
+MIT License
+-----------
+
+Copyright (c) 2016 Oliver Leics <oliver.leics@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,222 @@
+
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+
+module.exports = {
+  exists: exists,
+
+  isFile: isFile,
+  isFileStrict: isFileStrict,
+  readFile: readFile,
+  readJsonFile: readJsonFile,
+  writeFile: writeFile,
+  writeJsonFile: writeJsonFile,
+  unlink: unlink,
+
+  isFolder: isFolder,
+  isFolderStrict: isFolderStrict,
+  readFolder: readFolder,
+  mkFolder: mkFolder,
+  rmFolder: rmFolder,
+  rimrafFolder: rimrafFolder,
+  rmEmptyFolder: rmEmptyFolder,
+  rmEmptyFolderTail: rmEmptyFolderTail,
+
+  symlink: symlink,
+  readlink: readlink,
+  isSymlink: isSymlink
+};
+
+// Paths
+
+function exists(p) {
+  return new Promise(function(resolve, reject) {
+    fs.exists(p, function(exists){
+      resolve(exists);
+    });
+  });
+}
+
+// Files
+
+function isFile(p) {
+  return new Promise(function(resolve, reject) {
+    fs.stat(p, function(err, s){
+      if(err) return reject(err);
+      resolve(s.isFile());
+    });
+  });
+}
+
+function isFileStrict(p) {
+  return new Promise(function(resolve, reject) {
+    fs.lstat(p, function(err, s){
+      if(err) return reject(err);
+      resolve(s.isFile());
+    });
+  });
+}
+
+function readFile(p) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(p, function(err, d){
+      if(err) return reject(err);
+      resolve(d);
+    });
+  });
+}
+
+function readJsonFile(p) {
+  return readFile(p)
+    .then(function(d){
+      try {
+        d = JSON.parse(d.toString());
+      } catch(err) {
+        return reject(err);
+      }
+      resolve(d);
+    })
+  ;
+}
+
+function writeFile(p, data) {
+  return new Promise(function(resolve, reject) {
+    fs.writeFile(p, data, function(err){
+      if(err) return reject(err);
+      resolve();
+    });
+  });
+}
+
+function writeJsonFile(p, data) {
+  data = JSON.stringify(data, null, '  ')+'\n';
+  return writeFile(p, data);
+}
+
+function unlink(p) {
+  return new Promise(function(resolve, reject) {
+    fs.unlink(p, function(err){
+      if(err) return reject(err);
+      resolve();
+    });
+  });
+}
+
+// Folders
+
+function isFolder(p) {
+  return new Promise(function(resolve, reject) {
+    fs.stat(p, function(err, s){
+      if(err) return reject(err);
+      resolve(s.isDirectory());
+    });
+  });
+}
+
+function isFolderStrict(p) {
+  return new Promise(function(resolve, reject) {
+    fs.lstat(p, function(err, s){
+      if(err) return reject(err);
+      resolve(s.isDirectory());
+    });
+  });
+}
+
+function readFolder(p) {
+  return new Promise(function(resolve, reject) {
+    fs.readdir(p, function(err, files){
+      if(err) return reject(err);
+      resolve(files);
+    });
+  });
+}
+
+function mkFolder(p, mode) {
+  return new Promise(function(resolve, reject) {
+    mkdirp(p, {
+      mode: mode != null ? mode : undefined
+    }, function(err){
+      if(err) {
+        console.error(err.stack||err);
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+function rmFolder(p) {
+  return new Promise(function(resolve, reject) {
+    fs.rmdir(p, function(err){
+      if(err && err.code !== 'ENOENT') return reject(err);
+      resolve();
+    })
+  });
+}
+
+function rimrafFolder(p) {
+  return new Promise(function(resolve, reject) {
+    rimraf(p, function(err){
+      if(err && err.code !== 'ENOENT') return reject(err);
+      resolve();
+    })
+  });
+}
+
+function rmEmptyFolder(p) {
+  return readFolder(p)
+    .catch(function(err){
+      if(err.code === 'ENOENT') return Promise.resolve([]);
+      return Promise.reject(err);
+    })
+    .then(function(files){
+      if(files.length) return false;
+      return rmFolder(p).then(function(){
+        return true;
+      });
+    })
+  ;
+}
+
+function rmEmptyFolderTail(p) {
+  return rmEmptyFolder(p).then(function(yes){
+    if(!yes) return;
+    p = p.split(path.sep);
+    p.pop();
+    if(p.length <= 1) return;
+    p = p.join(path.sep);
+    return rmEmptyFolderTail(p);
+  });
+}
+
+// Symbolic Links
+
+function symlink(linkTo, p, type) {
+  if(type == null) type = 'junction';
+  return new Promise(function(resolve, reject) {
+    fs.symlink(linkTo, p, type, function(err){
+      if(err) return reject(err);
+      resolve();
+    });
+  });
+}
+
+function readlink(p) {
+  return new Promise(function(resolve, reject) {
+    fs.readlink(p, function(err, linkTo){
+      if(err) return reject(err);
+      resolve(linkTo);
+    });
+  });
+}
+
+function isSymlink(p) {
+  return new Promise(function(resolve, reject) {
+    fs.lstat(p, function(err, s){
+      if(err) return reject(err);
+      return resolve(s.isSymbolicLink());
+    });
+  });
+}
